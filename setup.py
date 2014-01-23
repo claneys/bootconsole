85a29f4867f8897377b7c0b3b57f6bc68fa138e0
Forlot Romain <rforlot@yahoo.com>
2014-01-23 23:27:35 +0100

    Add: intermediate menu to ask if we keep net conf. Add: sic_seal script to seal a VM a generate a UUID for a serial number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 import bootconsole
 
 setup (name = "bootconsole",
-       version = "1.1",
+       version = "1.2",
        description = "Boot Ncurses Console configuration",
        include_package_data=True,
        author = "Romain Forlot",
