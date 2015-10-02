82bb1dfac9733766b1226473342ad5bc310646cd
rfor <rfor@eb2ef830-13d2-490d-b0bb-9826b204079b>
2015-10-02 13:38:59 +0000

    Increment version
    git-svn-id: http://svn.syleps.fr/svn/ssi/VMware scripts/Linux/bootconsole/trunk@583 eb2ef830-13d2-490d-b0bb-9826b204079b

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 import bootconsole
 
 setup (name = "bootconsole",
-       version = "1.25",
+       version = "1.26",
        description = "Boot Ncurses Console configuration",
        include_package_data=True,
        author = "Romain Forlot",
