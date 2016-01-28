d7de57cbfa2580ba2d47fc4954fb9a4dd73cd5a6
rfor <rfor@eb2ef830-13d2-490d-b0bb-9826b204079b>
2016-01-28 15:04:36 +0000

    Tagged a new version 1.29
    git-svn-id: http://svn.syleps.fr/svn/ssi/VMware scripts/Linux/bootconsole/trunk@600 eb2ef830-13d2-490d-b0bb-9826b204079b

--- packaging/redhat/bootconsole_el7.spec
+++ packaging/redhat/bootconsole_el7.spec
@@ -1,5 +1,5 @@
 %define name bootconsole
-%define version 1.28
+%define version 1.29
 %define release 10.el7
 
 Summary: Boot Ncurses Console configuration
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 import bootconsole
 
 setup (name = "bootconsole",
-       version = "1.28",
+       version = "1.29",
        description = "Boot Ncurses Console configuration",
        include_package_data=True,
        author = "Romain Forlot",
